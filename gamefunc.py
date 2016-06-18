112dfa652094370e84d406662875ec851d64b35c
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2016-06-18 06:55:01 -0700

    built test model complete api

---
+++ gamefunc.py
@@ -0,0 +1,31 @@
+'''The file provides utility functions for the operation of the game.'''
+import random
+import sys
+
+def rand_english_word(length):
+    """takes the length of a desired word, and returns a dictionary word of
+    that length 0 indexed. Word is represented as a list of characters."""
+
+    potential_word = []
+    # run through a dictionary, return a random words of the defined length.
+    with open('data/dictionary.dat') as f:
+        wordlist = f.readlines()
+        for word in wordlist:
+            if len(word) == length + 1:
+                potential_word.append(word)
+    # count how many words meet the criteria, and use that in the range when
+    # generating a random int, then make sure the game settings match the data:
+    word_number = len(potential_word)
+    try:
+        word = potential_word[random.randint(1,word_number)]
+    except ValueError as e:
+        print 'Game Config Error: No words of specified length in word list'
+        sys.exit(1)
+
+    # verified word, convert to list of chars and del the newline:
+    word = potential_word[random.randint(1,word_number)]
+    # debug:
+    word = 'abcdef\n'
+    final_word = list(word)
+    del final_word[-1]
+    return final_word
---
+++ hangman.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# Local implementation of hangman game to aid in API design.
+
+import gamefunc
+import sys
+
+# Game Settings:
+WORD_SIZE = 6
+MAX_TRIES = 5
+DEAD = '''
+ ___________.._______
+| .__________))______|
+| | / /      ||
+| |/ /       ||
+| | /        ||.-''.
+| |/         |/  _  \
+| |          ||  `/,|
+| |          (\\`_.'
+| |         .-`--'.
+| |        /Y . . Y\
+| |       // |   | \\
+| |      //  | . |  \\
+| |     ')   |   |   (`
+| |          ||'||
+| |          || ||
+| |          || ||
+| |          || ||
+| |         / | | \
+""""""""""|_`-' `-' |"""|
+|"|"""""""\ \       '"|"|
+| |        \ \        | |
+: :         \ \       : :  sk
+. .          `'       . .
+
+'''
+
+class hangman():
+    def __init__(self):
+        '''Create a game'''
+        self.game_word = gamefunc.rand_english_word(WORD_SIZE)
+        self.max_tries = MAX_TRIES
+        self.num_guesses = 0
+        self.completed_letters = self.guess_status()
+
+    def guess_status(self, completed_letters=[]):
+        if completed_letters == []:
+            for i in range(WORD_SIZE):
+                completed_letters.append('_')
+        return completed_letters
+
+    def make_guess(self):
+        '''Accept a character, sanitize.'''
+        # check that only one character is entered, and numbers are rejected:
+        letter = 'aa'
+        while len(letter) > 1:
+            letter = raw_input('Enter your guess: ')
+            if len(letter) >= 2:
+                print "only 1 character allowed"
+            if letter[0] in '1234567890':
+                print 'Numbers not valid.'
+                letter = 'aa'
+        return letter
+
+    def verify_guess(self, guess):
+        # check to see if guess is in game word.
+        if guess in self.game_word:
+            location = self.game_word.index(guess)
+            self.completed_letters[location] = guess
+            return True
+        else:
+            return False
+
+    def play(self):
+        game = hangman()
+        print "Welcome to Paul's Hangman!\n"
+        print "A word has been generated, its {} characters long.\
+                                                             ".format(WORD_SIZE)
+        wrong_guesses = 0
+        while wrong_guesses < self.max_tries:
+            print "current status of word:"
+            print self.completed_letters
+            print 'Guess a letter!'
+            guess = self.make_guess()
+            if self.verify_guess(guess):
+                print 'Correct Guess!'
+                if '_' not in self.completed_letters:
+                    print 'Game over! You Win!'
+                    sys.exit(0)
+            else:
+                print 'Incorrect Guess!'
+                wrong_guesses += 1
+        print DEAD
+        print "Max Tries Exceeded, you lose."
+
+
+if __name__ == '__main__':
+    # Fire the gameloop:
+    hangman.play(hangman())
