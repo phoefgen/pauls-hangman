049adfd22e3228e0408d4caaed9d039281b34531
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2016-06-25 02:50:38 -0700

    feature complete

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import logging
 import webapp2
 from google.appengine.api import mail, app_identity
 from api import PaulsHangmanApi
+from models import Game
 
 from models import User
 
@@ -17,16 +18,27 @@ class SendReminderEmail(webapp2.RequestHandler):
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
+        # from the list off all users with email addresses, generate a list of
+        # all players with incomplete games:
+        contactable_users = []
         for user in users:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Pauls Hangman!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
+            game_search = Game.query(Game.user == user.key,
+                                     Game.game_over == False)
+            if len(game_search.fetch()) > 0:
+                contactable_users.append(user)
 
+        # Spam users with incomplete games.
+        for user in contactable_users:
+                subject = 'You have incomplete Hangman games!!'
+                body = ('Hello {}, stop what your doing and finish your game'
+                                                             ).format(user.name)
+
+                # This will send test emails, the arguments to send_mail are:
+                # from, to, subject, body
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                                                                   user.email,
+                                                                   subject,
+                                                                   body)
 
 class UpdateAverageMovesRemaining(webapp2.RequestHandler):
     def post(self):
